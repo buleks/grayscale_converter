96b38dbd7af6db8a1c41c41bfcae533724b75b26
buleks <buleks@buleks.krak.in.tarnow.pl.1.168.192.in-addr.arpa>
2016-05-29 16:09:30 +0200

    Poprawienie konwersji na gifa bo robila skale szarosci

--- ImageGIF.cpp
+++ ImageGIF.cpp
@@ -7,7 +7,9 @@ void ImageGIF::save(std::string name)
 	if(bitmap != nullptr)
 	{
 		FIBITMAP *bit8_bitmap;
-		bit8_bitmap = FreeImage_ConvertTo8Bits(bitmap);
+		//bit8_bitmap = FreeImage_ConvertTo8Bits(bitmap);
+		bit8_bitmap = FreeImage_ColorQuantize(bitmap,FIQ_WUQUANT);
+
 		FreeImage_Save(FIF_GIF,bit8_bitmap,filename.c_str(),0);
 		FreeImage_Unload(bit8_bitmap);	
 	}
