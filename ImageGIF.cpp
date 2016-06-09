b532d4eeff750e375715d20d4db1c1ea1f90cfc9
buleks <buleks@gmail.com>
2016-06-09 12:28:27 +0200

    Poprawienie gifa gdy chcemy skale szarosci

--- ImageGIF.cpp
+++ ImageGIF.cpp
@@ -1,14 +1,22 @@
 #include "ImageGIF.h"
 #include <FreeImage.h>
-
+#include <iostream>
 void ImageGIF::save(std::string name)
 {
 	std::string filename = name+".gif";
 	if(bitmap != nullptr)
 	{
 		FIBITMAP *bit8_bitmap;
-		//bit8_bitmap = FreeImage_ConvertTo8Bits(bitmap);
-		bit8_bitmap = FreeImage_ColorQuantize(bitmap,FIQ_WUQUANT);
+		int colors = FreeImage_GetColorsUsed(bitmap);
+		if(colors == 256)
+		{
+			bit8_bitmap = FreeImage_ConvertTo8Bits(bitmap);
+		}else
+		{
+				bit8_bitmap = FreeImage_ColorQuantize(bitmap,FIQ_WUQUANT);
+		}
+		
+	
 
 		FreeImage_Save(FIF_GIF,bit8_bitmap,filename.c_str(),0);
 		FreeImage_Unload(bit8_bitmap);	
